2cbd9ea843c62c424448610a0d90351bd8937ae6
Alvaro <ajsabini@gmail.com>
2022-08-25 19:59:26 -0700

    1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bi/python3
+# create .tgz(compress) before seding
+
+from fabric.api import local
+from datetime import datetime
+from os import path
+
+def do_pack():
+    """ generate compress file  """
+
+    fecha = datetime.now().strftime("%Y%m%d%H%M%S")
+    nombre_arch = "versions/web_static_" + fecha + ".tgz"
+    local("mkdir -p versions")
+
+    try:
+        local("tar -cvzf " + fecha + " web_static")
+        return(nomre_arch)
+    except Exception:
+        return None
