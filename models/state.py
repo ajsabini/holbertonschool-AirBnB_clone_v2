88946e8e3b327fbfebaacb9df7774684352fd5a4
ajsabini <ajsabini@gmail.com>
2022-09-06 02:03:45 +0000

    saa

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.city import City
 import models
 import os
 
@@ -16,11 +17,11 @@ class State(BaseModel, Base):
         cities = relationship("City", cascade="all,delete", backref="state")
     elif os.getenv('HBNB_TYPE_STORAGE') == 'fs':
         name = ''
- 
+
         @property
         def cities(self):
             citylist = []
-            city_dict = models.storage.all(models.city.City)
+            city_dict = models.storage.all(City)
             for key, value in city_dict.items():
                 if value.state_id == self.id:
                     citylist.append(value)
