e491cb88386962519caf979e91f5802a7b170b78
ajsabini <ajsabini@gmail.com>
2022-09-06 02:33:10 +0000

    c

--- models/city.py
+++ models/city.py
@@ -7,8 +7,13 @@ from models.state import State
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
     
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities", cascade="all, delete")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete")
+    elif os.getenv('HBNB_TYPE_STORAGE') == 'fs':
+        state_id = ''
+        name = ''
+        plaes = []
